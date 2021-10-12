a7fdee3ed67c0bda01d91e0714f3d24c3c2055f1
Eugene Zykin <porridge52@gmail.com>
2021-10-12 12:11:55 +0300

    some swagger fix

--- taxi_company/src/main/java/ru/digitalleague/taxi_company/entity/OrderDetails.java
+++ taxi_company/src/main/java/ru/digitalleague/taxi_company/entity/OrderDetails.java
@@ -1,29 +1,38 @@
 package ru.digitalleague.taxi_company.entity;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.time.OffsetDateTime;
 
 @Data
+@ApiModel
 public class OrderDetails {
-
+    @ApiModelProperty("Id заказа")
     private Long orderId;
 
     /**
      * Идентификатор клиента.
      */
-
+    @ApiModelProperty("Номер телефона клиента")
     private Long clientNumber;
 
+    @ApiModelProperty("ИД водителя")
     private Long driverId;
 
+    @ApiModelProperty("Точка отправления")
     private String startPointAddress;
 
+    @ApiModelProperty("Пункт назначения")
     private String endPointAddress;
 
+    @ApiModelProperty("Время начала поездки")
     private OffsetDateTime startTrip;
 
+    @ApiModelProperty("Время окончания поездки")
     private OffsetDateTime endTrip;
 
+    @ApiModelProperty("Город")
     private String city;
 }
--- taxi_company/src/main/java/ru/digitalleague/taxi_company/entity/TaxiDriverInfoModel.java
+++ taxi_company/src/main/java/ru/digitalleague/taxi_company/entity/TaxiDriverInfoModel.java
@@ -1,49 +1,61 @@
 package ru.digitalleague.taxi_company.entity;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.time.OffsetDateTime;
 import java.util.List;
 
 @Data
+@ApiModel
 public class TaxiDriverInfoModel {
-
+    @ApiModelProperty("ИД водителя")
     private Long driverId;
 
     /**
      * Фамилия.
      */
+    @ApiModelProperty("Фамилия")
     private String lastName;
 
     /**
      * Имя.
      */
+    @ApiModelProperty("Имя")
     private String firstName;
 
     /**
      * Отчество.
      */
+    @ApiModelProperty("Отчество")
     private String middleName;
 
     /**
      * Уровень.
      */
+    @ApiModelProperty("Уровень авто")
     private int level;
 
     /**
      * Модель авто (должна быть ENUM).
      */
+    @ApiModelProperty("Модель машины")
     private String carModel;
 
     /**
      * Дата создания.
      */
+    @ApiModelProperty("Время найма")
     private OffsetDateTime createDttm;
 
     //Оценки водителя для расчета среднего арифметического для globalRating
+    @ApiModelProperty("Оценки водителя для расчета средней арифметической")
     private List <Integer> rating;
 
+    @ApiModelProperty("Средняя оценка")
     private int globalRating;
 
+    @ApiModelProperty("Статус водителя")
     private boolean isBusy;
 }
